3ace198cc4c7d85d318689fb95d9c1ec5ceb5a05
Alan Silva <alansilalcantara@gmail.com>
2024-01-02 12:36:28 -0300

    primeiro commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -68,7 +68,7 @@ export const Contact = () => {
                     <Col size={12} sm={6} className="px-1">
                       <input type="text" value={formDetails.lasttName} placeholder="Último nome" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
-                    <Col size={12} sm={6} className="px-1">
+                    <Col size={12} sm={6} className="px-1">  
                       <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
